ceef2e8c110fc2db379469939f2f79794af091c3
Hannah Hironaka <hmmh@google.com>
2024-10-17 14:50:59 -0400

    Reapply "Reapply "Updates to cover prompts""
    This reverts commit 2ab546bf23d8a7312525740cf879dd06ca0312c4.

--- gen_cover.py
+++ gen_cover.py
@@ -16,17 +16,16 @@ vertexai.init(project="stations-243022", location="us-central1")
 
 imagen_model = ImageGenerationModel.from_pretrained("imagen-3.0-fast-generate-001")
 
-SYSTEM_INSTRUCTION = """You are a helpful and creative thumbnail designer. 
-Create an image of PROMPT with the \"TITLE\" written in a creative font across the center that is bold and eye-catching.
-Make sure the words are human-readable and displayed exactly as written. 
+PROMPT = """You are a helpful and creative thumbnail designer. 
+Create an image of <PROMPT> with the \"TITLE\" written in a creative font across the center that is bold and eye-catching.
+Make sure the words in <TITLE> are human-readable and displayed exactly as written. 
 
-# Update me.
-TITLE = "Pawsome Pals"
-PROMPT = "A cute dogs and cats"
-PROMPT_TEMPLATE = "Display '{}' in a bold and eye-catching font. Make the background {}"
+<TITLE>Pawsome Pals</TITLE>
+<PROMPT>A cute dogs and cats</PROMPT>
+"""
 
 images = imagen_model.generate_images(
-    prompt=PROMPT_TEMPLATE.format(TITLE, PROMPT),
+    prompt=PROMPT,
     number_of_images=4,
     aspect_ratio="3:4",
     safety_filter_level="block_some",
